38d1b13b7c181c8296de5d3498be4e81392e75e0
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-24 22:06:37 +0530

    Fetching Related Products

--- OnlineShop/controllers/admin.js
+++ OnlineShop/controllers/admin.js
@@ -47,19 +47,35 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
-      if (!product) {
-        return res.redirect("/");
-      }
-      res.render("admin/edit-product", {
-        pageTitle: "Edit Product",
-        path: "/admin/edit-product",
-        editing: editMode,
-        product: product,
-      });
-    })
-    .catch((e) => console.log(e));
+
+  // to use req.user we have to set a middleware in app.js. and in this we will get getProducts method.
+  req.user.getProducts({where : {id:prodId}}).then((products) => {
+    const product = products[0];
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
+  })
+  .catch((e) => console.log(e));
+
+  // Product.findByPk(prodId)
+  //   .then((product) => {
+  //     if (!product) {
+  //       return res.redirect("/");
+  //     }
+  //     res.render("admin/edit-product", {
+  //       pageTitle: "Edit Product",
+  //       path: "/admin/edit-product",
+  //       editing: editMode,
+  //       product: product,
+  //     });
+  //   })
+  //   .catch((e) => console.log(e));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -85,15 +101,25 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
-    .then((products) => {
-      res.render("admin/products", {
-        prods: products,
-        pageTitle: "Admin Products",
-        path: "/admin/products",
-      });
-    })
-    .catch((e) => console.log(e));
+
+  // using request.user.getProducts() instead of Model.findAll()
+  req.user.getProducts().then((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  })
+  .catch((e) => console.log(e));
+  // Product.findAll()
+  //   .then((products) => {
+  //     res.render("admin/products", {
+  //       prods: products,
+  //       pageTitle: "Admin Products",
+  //       path: "/admin/products",
+  //     });
+  //   })
+  //   .catch((e) => console.log(e));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
