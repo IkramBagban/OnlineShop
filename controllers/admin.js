62a02ed471a5f45303e6004d96436ab643d65497
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-16 15:46:10 +0530

    Solved 'then' undefined' replace then/catch with try/catch

--- controllers/admin.js
+++ controllers/admin.js
@@ -121,11 +121,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-
-  Product.deleteById(prodId)
-    .then((result) => {
-      console.log("Product Has Been Deleted");
-      res.redirect("/admin/products");
-    })
-    .catch((e) => console.log("Got An Error While Deleting Product", e));
+  try {
+    Product.deleteById(prodId);
+    res.redirect("/admin/products");
+  } catch (err) {
+    console.log(err);
+  }
 };
