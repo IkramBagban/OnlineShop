625bb004e166e5105fe5608495a82e6076a1b4bc
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-17 13:17:01 +0530

    getting products using "find" method provided by mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,8 @@
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  // find method is provided by mongoose. it gives us the products(data)
+  Product.find() 
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
@@ -37,7 +38,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("shop/index", {
         prods: products,
