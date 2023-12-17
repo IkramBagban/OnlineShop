ee51cc227864d25a1e4549d1460e99e7397b20f0
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-17 12:37:13 +0530

    working with mongoose

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const path = require("path");
-
 const express = require("express");
+
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-const User = require("./models/user");
 const errorController = require("./controllers/error");
-
-const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
@@ -21,21 +19,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("657d7d1e437fd6aa816d5170")
-    .then((user) => {
-      const USER = new User(user.name, user.email, user.cart, user._id)
-      req.user = USER;
-      next();
-    })
-    .catch((err) => console.log(err));
+  const USER = { username: "ikram", email: "bagbanikram@gmail.com" };
+  req.user = USER;
+  next();
 });
-
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(2000);
-});
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Server is running on the PORT", 2000);
+    app.listen(2000);
+  })
+  .catch((err) => console.log(err));
