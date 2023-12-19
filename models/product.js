3800bf2ac39ffa700e902191956ce2a0f304807e
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-19 18:05:38 +0530

    created Order Model with user refrence

--- models/product.js
+++ models/product.js
@@ -22,7 +22,7 @@ const ProductSchema = new Schema({
     required: true,
   },
   userId : {
-    type : Schema.ObjectId,
+    type : Schema.Types.ObjectId,
     ref : 'User',
     required : true
   }
