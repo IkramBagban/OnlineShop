4421a65ad19a001eeb119d702613fc4534e2e7e7
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-23 21:08:41 +0530

    if user is authenticated than showing some components else hiding

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,31 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
+  const isLoggedIn =
+    req.get("Cookie").split(";")[0].trim().split("=")[1] === "true";
   console.log(req.session);
   console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: req.session.isLoggedIn
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
 // Storing data in a session
 exports.postLogin = (req, res, next) => {
-    User.findById("65806c272c1279a21240f881")
-      .then((user) => {
-        req.session.isLoggedIn = true;
-        req.session.user = user;
-        res.redirect("/");
-      })
-      .catch((err) => console.log(err));
+  User.findById("65806c272c1279a21240f881")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
-    req.session.destroy(err=>{
-      console.log(err)
-      res.redirect('/')
-    })
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
