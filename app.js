5d3549482223a8aecf6c9004cb9a35aa5e3c088c
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-25 18:22:59 +0530

    using connect-flash for errorMessage

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const mongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
+const flash = require("connect-flash");
 
 require("dotenv").config();
 
@@ -39,6 +40,7 @@ app.use(
 );
 
 app.use(csrfProtection);
+app.use(flash())
 
 app.use((req, res, next) => {
   if (!req.session.user) {
