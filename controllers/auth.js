cec83c66b6a368f0b0c46935637a999d3adafa3a
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-01 22:34:46 +0530

    using express-validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
+const {validationResult} = require('express-validator')
+
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -83,6 +85,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    console.log(errors.array()[0].msg)
+    return res.status(422).render('auth/signup',{
+      path:'signup',
+      pageTitle:'signup',
+      errorMessage : errors.array()[0].msg || "error"
+    })
+  }
 
   User.findOne({ email: email })
     .then((user) => {
