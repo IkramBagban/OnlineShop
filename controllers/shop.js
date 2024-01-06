030a7e752fa8d76933e023e4c44a3b0ff60dddf9
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-06 13:35:44 +0530

    authorized

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,18 +1,19 @@
-const path = require('path')
-const fs = require('fs')
-const Product = require('../models/product');
-const Order = require('../models/order');
+const path = require("path");
+const fs = require("fs");
+const Product = require("../models/product");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
-    .then(products => {
+    .then((products) => {
       console.log(products);
-      res.render('shop/product-list', {
+      res.render("shop/product-list", {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products',
+        pageTitle: "All Products",
+        path: "/products",
       });
-    }).catch(err => {
+    })
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -22,13 +23,14 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then(product => {
-      res.render('shop/product-detail', {
+    .then((product) => {
+      res.render("shop/product-detail", {
         product: product,
         pageTitle: product.title,
-        path: '/products',
+        path: "/products",
       });
-    }).catch(err => {
+    })
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -37,16 +39,17 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   Product.find()
-    .then(products => {
-      res.render('shop/index', {
+    .then((products) => {
+      res.render("shop/index", {
         prods: products,
-        pageTitle: 'Shop',
-        path: '/',
+        pageTitle: "Shop",
+        path: "/",
         // These variables are now available through res.locals
         // isAuthenticated: req.session.isLoggedIn,
         // csrfToken : req.csrfToken()
       });
-    }).catch(err => {
+    })
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -55,15 +58,16 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
-    .then(user => {
+    .populate("cart.items.productId")
+    .then((user) => {
       const products = user.cart.items;
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
         products: products,
       });
-    }).catch(err => {
+    })
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -73,12 +77,12 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
-    .then(product => {
+    .then((product) => {
       return req.user.addToCart(product);
     })
-    .then(result => {
+    .then((result) => {
       console.log(result);
-      res.redirect('/cart');
+      res.redirect("/cart");
     });
 };
 
@@ -86,10 +90,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
     .removeFromCart(prodId)
-    .then(result => {
-      res.redirect('/cart');
+    .then((result) => {
+      res.redirect("/cart");
     })
-    .catch(err => {
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -98,26 +102,27 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
-    .then(user => {
-      const products = user.cart.items.map(i => {
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
       });
       const order = new Order({
         user: {
           email: req.user.email,
-          userId: req.user
+          userId: req.user,
         },
-        products: products
+        products: products,
       });
       return order.save();
     })
-    .then(result => {
+    .then((result) => {
       return req.user.clearCart();
     })
     .then(() => {
-      res.redirect('/orders');
-    }).catch(err => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -125,35 +130,50 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({ 'user.userId': req.user._id })
-    .then(orders => {
-      console.log(orders)
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
+  Order.find({ "user.userId": req.user._id })
+    .then((orders) => {
+      console.log(orders);
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
         orders: orders,
       });
-    }).catch(err => {
+    })
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
     });
 };
 
-
-exports.getInvoice = (req,res,next) =>{
-  console.log('invoicePath')
+exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  console.log('orderId',orderId)
-  const invoiceName = 'invoice-' + orderId + '.pdf'
-  console.log('invoiceName',invoiceName)
-  const invoicePath = path.resolve('data', 'invoices', invoiceName)
-  console.log('invoicePath',invoicePath)
-      fs.readFile(invoicePath, (err, data)=>{
-        if(err){
-          console.log('err')
-          return next(err)
+
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("no order found"));
+      }
+
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unothorized"));
+      }
+
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.resolve("data", "invoices", invoiceName);
+      fs.readFile(invoicePath, (err, data) => {
+        if (err) {
+          console.log("err");
+          return next(err);
         }
-        res.send(data)
-      })
-}
+
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader(
+          "Content-Disposition",
+          'attachment; filename="' + invoiceName + '" '
+        );
+        res.send(data);
+      });
+    })
+    .catch((err) => {next(err)});
+};
