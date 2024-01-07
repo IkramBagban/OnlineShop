4cba2c8dbb53f23ce447a93f85751742b733afae
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-01-07 13:54:25 +0530

    added checkout page

--- controllers/shop.js
+++ controllers/shop.js
@@ -59,37 +59,6 @@ exports.getProduct = (req, res, next) => {
     });
 };
 
-// exports.getIndex = (req, res, next) => {
-//   const page = +req.query.page || 1;
-//   console.log("page", page);
-//   let totalItems;
-//   Product.find()
-//     .countDocuments((numProducts) => {
-//       console.log(numProducts);
-//       totalItems = numProducts;
-//       return Product.find()
-//         .skip((page - 1) * ITEMS_PER_PAGE)
-//         .limit(ITEMS_PER_PAGE);
-//     })
-//     .then((products) => {
-//       res.render("shop/index", {
-//         prods: products,
-//         pageTitle: "Shop",
-//         path: "/",
-//         currentPage: page,
-//         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-//         hasPreviousPage: page > 1,
-//         nextPage: page + 1,
-//         previousPage: page - 1,
-//         lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
-//       });
-//     })
-//     .catch((err) => {
-//       const error = new Error(err);
-//       error.httpStatusCode = 500;
-//       return next(error);
-//     });
-// };
 exports.getIndex = (req, res, next) => {
   const page = +req.query.page || 1;
   console.log("Page:", page);
@@ -162,6 +131,29 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .removeFromCart(prodId)
     .then((result) => {
       res.redirect("/cart");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p =>{
+        total += p.quantity * p.productId.price
+      })
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum : total
+      });
     })
     .catch((err) => {
       const error = new Error(err);
